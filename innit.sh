eb6a360a3f3b85a7fb61bddca77dbb4c75dcb0ed
Agusb04 <fbeltrame@fi.uba.ar>
2024-11-07 12:52:39 -0300

    Update innit.sh

--- innit.sh
+++ innit.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-project_folder_name=$1
-
-if python3 --version ; then
-    echo "Python installed"
-else
-    echo "Python will be installed"
-    sudo apt install python3
-fi
-
-if pip3 --version ; then
-   echo "Pip installed"
-else
-   echo "Pip will be installed"
-   sudo apt install python3-pip
-fi
-
-sudo apt install python3.10-venv
-
-mkdir $project_folder_name
-cd $project_folder_name
-
 python3 -m venv .venv
 
 . .venv/bin/activate
