496c74475bda9a7d1a865a7a53c618c5655fed90
Agustin Beltrame <fbeltrame@fi.uba.ar>
2024-11-10 13:02:19 -0300

    compraRealizada.html id 34

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from user_agents import parse
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+
+@app.route('/compraRealizada/<id>')
+def compraRealizada(id):
     user_agent = parse(request.headers.get('User-Agent'))
     is_mobile = user_agent.is_mobile  
-    return render_template("base.html", is_mobile=is_mobile)
+    return render_template("compraRealizada.html", is_mobile=is_mobile, id=id)
 
 
 if __name__ == '__main__':
