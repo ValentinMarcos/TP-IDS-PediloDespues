3a6254a80cbce95c0687cb529d5f029a9fd724ef
Agusb04 <fbeltrame@fi.uba.ar>
2024-11-18 17:31:13 -0300

    Delete FRONT directory

--- FRONT/README.md
+++
@@ -1,1 +0,0 @@
-# TP-IDS-PediloDespues
--- FRONT/static/js/procesoPago.js
+++
@@ -1,9 +0,0 @@
-// Obtener el carrito desde el Local Storage
-const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-
-// Convertir el carrito a JSON
-const carritoInput = document.querySelector("#carritoInput");
-if (carritoInput) {
-    const carritoJSON = JSON.stringify(carrito);
-    carritoInput.value = carritoJSON;
-}
