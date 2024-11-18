fd269ec97fd950fa78f2c0a0497e6191c811d7b8
Agustin Beltrame <fbeltrame@fi.uba.ar>
2024-11-18 18:45:35 -0300

    Actualizacion primer endpoint ver productos

--- BACK/api.py
+++ BACK/api.py
@@ -39,25 +39,23 @@ def ver_productos():
     if not productos:
         return jsonify({})
     
-   
     productos_por_categoria = {}
 
-   
     for producto in productos:
-    
         id_producto, descripcion, precio, categoria = producto 
-        
         
         if categoria not in productos_por_categoria:
             productos_por_categoria[categoria] = []
         
         
+        descripcion_img = descripcion.lower().replace(" ", "")
+        
         productos_por_categoria[categoria].append({
             'nombre': descripcion,
+            'img': f"static/images/{descripcion_img}.jpg",  
             'precio': precio
         })
     
-   
     return jsonify(productos_por_categoria)
 
 def agregar_producto(data):
