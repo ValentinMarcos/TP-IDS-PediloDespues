bf5c68d9dfc0075d3b53400975ab3d12dc32c161
leonel-r2 <lrolon@fi.uba.ar>
2024-11-19 03:07:21 -0300

    agrego el endpoint a la API /cargardatos , carga una serie de datos en la base de datos para poder ejecutar la DEMO

--- BACK/app.py
+++ BACK/app.py
@@ -19,5 +19,25 @@ def ver_productos():
     
     return jsonify(productos_por_categoria)
 
+@app.route("/cargardatos")
+def cargar_datos():
+    cvs = [
+        ['Coca cola', '3000.00', 'bebidas' ], 
+        ['Vino tinto', '6000.00', 'bebidas'], 
+        ['Milanesa de pollo', '7000.00', 'carnes'],
+        ['Pollo al horno', '7000.00', 'carnes'], 
+        ['Papas fritas', '2000.00', 'acompañamientos'],
+        ['Ensalada mixta', '2000.00', 'acompañamientos'], 
+        ['Flan', '4000.00', 'postres'],
+        ['Helado', '4000.00', 'postres']        
+    ]
+
+    for Descripcion, Precio, Categoria in cvs:
+        q.ejecutarSQL(q.PRODUCTOS_ADD, {"Descripcion" : Descripcion, "Precio": Precio, "Categoria" : Categoria})
+        
+    return jsonify({"mensaje" : "se cargaron los datos correctamente"})
+
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=5001,debug=True)
