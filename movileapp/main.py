29b5c4f900fa5dd5a46f5acd48505fab0d8c7596
Valentin Marcos <jvmarcos712003@gmail.com>
2024-11-21 17:39:38 -0300

    Iconos de tracking dinamicos ID 51

--- movileapp/main.py
+++ movileapp/main.py
@@ -1,8 +1,11 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
+from kivy.properties import ListProperty # type: ignore
+from kivy.clock import Clock
 from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDRaisedButton
+
 
 class MainScreen(Screen):
     pass
@@ -19,6 +22,9 @@ class TrackScreen(Screen):
     
 class MyApp(MDApp):
 
+    current_step = 0
+    state_icons = ListProperty(["truck-delivery", "", "", "", ""])  # Asi empieza, luego se actualiza dependiendo el estado del pedido
+
     def build(self):
         self.theme_cls.theme_style = 'Light'
         self.theme_cls.primary_palette = 'Red'
@@ -31,7 +37,23 @@ class MyApp(MDApp):
         self.sm.add_widget(SecondScreen(name='second_view'))
         self.sm.add_widget(TrackScreen(name='track_view'))
 
+        Clock.schedule_interval(self.simular_estado, 5)
+
         return self.sm
+
+    def simular_estado(self, dt):
+        if self.current_step < 5: 
+            self.estado_pedido(self.current_step)
+            self.current_step += 1
+
+    def estado_pedido(self, current_step):
+
+        steps = ["Autorizando", "Armando pedido", "En camino", "En su domicilio", "Pedido Entregado"]
+        self.state_icons = [
+            "check" if i < current_step else "truck-delivery" if i == current_step else ""
+            for i in range(len(steps))
+        ]
+
 
     def cambiar_vista(self):
         self.sm.current = "second_view"
